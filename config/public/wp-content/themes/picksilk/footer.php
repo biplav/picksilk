e33483c3fd9966695f3d16e3ea97b5aa610388c0
biplav saraf <biplav.saraf@example.com>
2014-10-12 04:57:10 +0530

    Footer improvements

--- config/public/wp-content/themes/picksilk/footer.php
+++ config/public/wp-content/themes/picksilk/footer.php
@@ -3,7 +3,16 @@
 <footer>
 <div class="footer">
       <div class="container">
-        <p class="text-muted">&copy; All rights reserved.</p>
+        <p class="text-muted">
+        	&copy; All rights reserved.
+        </p>
+        	<address class="text-muted">
+  			<strong>PickSilk Enterprise</strong><br>
+  				Bomannahalli<br>
+  				Bangalore, 560033<br>
+  			<abbr title="Phone">P:</abbr> (+91) 080-088812
+			</address>
+		
       </div>
     </div>
 </footer>
