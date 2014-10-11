32b27ba85ac571bc409f2d6f283acfa22eb6d6ba
biplav saraf <biplav.saraf@example.com>
2014-10-12 05:04:37 +0530

    Email in footer

--- config/public/wp-content/themes/picksilk/footer.php
+++ config/public/wp-content/themes/picksilk/footer.php
@@ -12,7 +12,10 @@
   				Bangalore, 560033<br>
   			<abbr title="Phone">P:</abbr> (+91) 080-088812
 			</address>
-		
+			<address>
+  			<strong>Help</strong><br>
+  			<a href="mailto:#">help@picksilk.com</a>
+			</address>
       </div>
     </div>
 </footer>
