4ac82522602cc6199b50967361fc2b969011b110
biplav <biplav.saraf@gmail.com>
2014-11-22 14:58:48 +0530

    fOOTER

--- config/public/wp-content/themes/picksilk/category_list.php
+++ config/public/wp-content/themes/picksilk/category_list.php
@@ -1,6 +1,5 @@
 <div class="category">
-  		
-  	
+
 <?php
     $args = array(
     'number'     => $number,
--- config/public/wp-content/themes/picksilk/footer.php
+++ config/public/wp-content/themes/picksilk/footer.php
@@ -3,19 +3,42 @@
 <footer>
 <div class="footer">
       <div class="container">
-        <p class="text-muted">
-        	&copy; All rights reserved.
-        </p>
-        	<address class="text-muted">
-  			<strong>PickSilk Enterprise</strong><br>
-  				Bomannahalli<br>
-  				Bangalore, 560033<br>
-  			<abbr title="Phone">P:</abbr> (+91) 080-088812
-			</address>
-			<address>
-  			<strong>Help</strong><br>
-  			<a href="mailto:#">help@picksilk.com</a>
-			</address>
+        <div class="row">
+          <div class="col-xs-6 col-md-4" style="background-color:#f4f4f4">
+            <div class='footer_menu'>
+              <ul class="nav nav-stacked">
+              <li>Home</li>
+              <li>About Us</li>
+              <li>Contact Us</li>
+              <li>Privacy Policy</li>
+              <li>Terms of Use</li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-xs-6 col-md-4" style="background-color:#9d9d9d">
+            <div class='footer_mid'>
+              <ul class="nav nav-stacked">
+              <li>How to Order</li>
+              <li>Billing and Payment</li>
+              <li>Shipping and Delivery</li>
+              <li>Returns and Exchange</li>
+              <li>FAQ's</li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-xs-6 col-md-4" style="background-color:#ff5960">
+            <div class='footer_last'>
+              <ul class="nav nav-stacked">
+              <li class="footer_last_text">080 - 40909240</li>
+              <li class="footer_last_text">help@picksilk.com</li>
+              <li class="footer_last_text">                         </li>
+              <li class="footer_last_text">Follow Us</li>
+              <li class="footer_last_text">fb</li>
+              </ul>
+            </div>
+            
+          </div>
+        </div>
       </div>
     </div>
 </footer>
